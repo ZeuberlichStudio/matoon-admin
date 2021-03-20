86bc07e88051c9a84554022236d37de0bdf50cce
Yaroslav Dymov <dymov.yar@gmail.com>
2021-03-20 20:01:35 +0300

    included dist

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './app';
-import { render } from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-render( <BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
